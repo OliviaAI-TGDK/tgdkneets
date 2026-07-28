6b7221ca33fb43d533505efe6238bc4fcf08f33d
Sean Tichenor aka Willow Tichenor <jtart2133014@gmail.com>
2026-07-28 11:16:41 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,9 +1,12 @@
 export default {
   async fetch(request) {
-    // change this to your real repo if needed
-    const r = await fetch("https://oliviaai-tgdk.github.io/");
-    return new Response(await r.text(), {
-      headers: { "content-type": "text/html;charset=UTF-8", "cache-control": "no-cache" }
+    return new Response(`
+
+`, {
+      headers: { 
+        "content-type": "text/html;charset=UTF-8",
+        "cache-control": "no-store"
+      }
     });
   }
 }
